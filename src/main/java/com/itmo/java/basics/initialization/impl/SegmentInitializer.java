c47e0f8cc97a7b6eca08623f7e5cf0c997b4ac54
DudkoMatt <github.dudko.matt@mail.ru>
2021-04-17 21:56:59 +0300

    fix: Trying not to use DatabaseInputStream

--- src/main/java/com/itmo/java/basics/initialization/impl/SegmentInitializer.java
+++ src/main/java/com/itmo/java/basics/initialization/impl/SegmentInitializer.java
@@ -5,15 +5,13 @@ import com.itmo.java.basics.index.impl.SegmentIndex;
 import com.itmo.java.basics.index.impl.SegmentOffsetInfoImpl;
 import com.itmo.java.basics.initialization.InitializationContext;
 import com.itmo.java.basics.initialization.Initializer;
-import com.itmo.java.basics.logic.DatabaseRecord;
 import com.itmo.java.basics.logic.Segment;
 import com.itmo.java.basics.logic.impl.SegmentImpl;
-import com.itmo.java.basics.logic.io.DatabaseInputStream;
 
+import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Optional;
 
 public class SegmentInitializer implements Initializer {
     /**
@@ -29,16 +27,20 @@ public class SegmentInitializer implements Initializer {
         SegmentIndex segmentIndex = new SegmentIndex();
         HashSet<String> presentKeys = new HashSet<>();
 
-        try (DatabaseInputStream databaseInputStream = new DatabaseInputStream(new FileInputStream(context.currentSegmentContext().getSegmentPath().toFile()))) {
-            while (databaseInputStream.available() > 0) {
-                long currentStreamPosition = databaseInputStream.getReadBytes();
-                Optional<DatabaseRecord> optionalDatabaseRecord = databaseInputStream.readDbUnit();
-                String lastKey = new String(databaseInputStream.getLastKeyObject());
+        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(context.currentSegmentContext().getSegmentPath().toFile()))) {
+            long currentStreamPosition = 0;
+            while (dataInputStream.available() > 0) {
+                int keySize = dataInputStream.readInt();
+                String lastKey = new String(dataInputStream.readNBytes(keySize));
+                int valueSize = dataInputStream.readInt();
+
+                if (valueSize != -1) {
+                    presentKeys.add(lastKey);
+                    dataInputStream.skip(valueSize);
+                }
+
                 segmentIndex.onIndexedEntityUpdated(lastKey, new SegmentOffsetInfoImpl(currentStreamPosition));
-                optionalDatabaseRecord.ifPresentOrElse(
-                        (databaseRecord) -> presentKeys.add(lastKey),
-                        () -> presentKeys.remove(lastKey)
-                );
+                currentStreamPosition += 4 * 2 + keySize + (valueSize == -1 ? 0 : valueSize);
             }
         } catch (IOException e) {
             throw new DatabaseException("Cannot create FileInputStream", e);
